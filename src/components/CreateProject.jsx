233886909a90c0d8fdafd4d56d5670ad8ad50c2c
alfndp <alfndp25@gmail.com>
2024-11-26 21:39:08 +0900

    edit description 버튼 추가

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -40,9 +40,9 @@ export default function CreateProject({ onChangeView, onSave }) {
           <h2 className="text-neutral-500 font-semibold mb-1">TITLE</h2>
           <input ref={inputTitle} type="text" name="title" className="bg-neutral-200 border-b-2 border-neutral-300 w-full h-8 mb-4"/>
           <h2 className="text-neutral-500 font-semibold mb-1">DESCRIPTION</h2>
-          <textarea ref={inputDescription} name="description" className="bg-neutral-200 border-b-2 border-neutral-300 w-full h-20 mb-4"></textarea>
+          <textarea ref={inputDescription} name="description" className="bg-neutral-200 border-b-2 border-neutral-300 w-full h-20 mb-4 pt-1 pl-2"></textarea>
           <h2 className="text-neutral-500 font-semibold mb-1">DUE DATE</h2>
-          <input ref={inputDueDate} type="date" name="dueDate" className="bg-neutral-200 border-b-2 border-neutral-300 w-full h-8"/>
+          <input ref={inputDueDate} type="date" name="dueDate" className="bg-neutral-200 border-b-2 border-neutral-300 w-52 h-8 pl-1"/>
         </div>
     </section>
   );
--- src/components/ProjectManage.jsx
+++ src/components/ProjectManage.jsx
@@ -4,48 +4,65 @@ let TASK_NO = 0;
 
 export default function ProjectManage({selectedProjectData, onTasksSave}) {
   const {projectId, title, description, dueDate, tasks} = selectedProjectData;
+  const [isEditing, setIsEditing] = useState(false);
+  const [editDesc, setEditDesc] = useState(null);
   const [newTasks, setNewTasks] = useState([]);
 
-  console.log({projectId, title, description, dueDate, tasks});
+  function handleSaveNewTasks() {
+    setNewTasks((prevTasks) => {
+      return []
+    });
+  }
 
-  // function saveNewTasks() {
-  //   const updateTasks = [
-  //     {
-  //       taskNo: TASK_NO,
-  //       taskContent: newTask.current.value
-  //     },
-  //     ...newTasks
-  //   ];
-  //
-  //   setNewTasks(updateTasks);
-  //   onTasksSave(updateTasks);
-  //
-  //   newTask.current.value = "";
-  //   TASK_NO++;
-  // }
-
-  return (
+    return (
     <section className="float-left grow pt-24 pl-12 pr-52">
-      <div className="flex justify-between mb-3">
-        <h1 className="text-3xl font-black text-neutral-700">{title}</h1>
+      <div className="flex justify-between mb-4">
+        <div className="flex items-end">
+          <h1 className="w-fit text-3xl font-black text-neutral-700 mr-6">{title}</h1>
+          <h2 className="w-fit font-semibold text-neutral-400">{dueDate}</h2>
+        </div>
+
         <div className="flex">
-          <button className="inline-block font-semibold text-neutral-500 border border-neutral-300 rounded-md w-20 py-1.5 mr-1">Update</button>
-          <button className="inline-block font-semibold text-red-900 border border-red-900 rounded-md w-20 py-1.5">Delete</button>
+          <button className="inline-block font-semibold text-neutral-500 border border-neutral-300 rounded-md w-20 h-fit py-1.5 mr-1"
+                  onClick={onTasksSave}
+          >
+            Update
+          </button>
+          <button className="inline-block font-semibold text-red-900 border border-red-900 rounded-md w-20 h-fit py-1.5"
+          >
+            Delete
+          </button>
         </div>
       </div>
-      <h2 className="font-semibold text-neutral-400 ">{dueDate}</h2>
-      <p className="mt-4 mb-6 text-lg font-semibold text-neutral-500 leading-7">
-        {description}
-      </p>
 
-      <hr className="mb-6 border-b-2 border-neutral-300" />
+      <div className="flex items-end mt-4">
+        {isEditing ? (
+          <textarea
+            className="w-[40rem] h-[72px] bg-neutral-200 border-neutral-300 pt-1"
+          >
+            {description}
+          </textarea>
+        ) : (
+          <p className="w-[40rem] font-semibold text-neutral-500 leading-6">
+            {description}
+          </p>
+        )}
+        <button
+          className="font-bold text-neutral-500 border-b-2 border-neutral-300 h-fit ml-3"
+          onClick={() => setIsEditing(!isEditing)}
+        >
+          {isEditing ? "Save Description" : "Edit Description"}
+        </button>
+      </div>
+
+      <hr className="mt-4 mb-6 border-b-2 border-neutral-300" />
 
       <h1 className="font-bold text-3xl text-neutral-700 mb-2">Tasks</h1>
       <input type="text" className="inline-block bg-neutral-200 rounded-md w-72 h-8"/>
       <button className="inline-block font-bold text-neutral-500 ml-4 mb-8">Add Task</button>
       {newTasks.length > 0 ? newTasks.map((task, key) =>
         <div key={key} className="flex justify-between grow h-20 bg-neutral-100">
-          <p className="flex items-center ml-4 font-bold text-neutral-700">{task.taskContent}</p>
+          <p className="flex items-center ml-4 font-bold text-neutral-700">{task}</p>
           <button className="inline-block mr-4 font-bold text-neutral-500">Clear</button>
         </div>
       ) : undefined}
